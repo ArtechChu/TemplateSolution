d62710f4bab32bc33e5b6fe19c4c8e43f091893f
ArtechChu <kamui_shiron@hotmail.com>
2018-12-10 23:55:15 +0800

    添加模板.template信息

--- src/Artech.XXX.WebApi/Controllers/ValuesController.cs
+++ src/Artech.XXX.WebApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Artech.XXX.WebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Artech", "Chu" };
         }
 
         // GET api/values/5
